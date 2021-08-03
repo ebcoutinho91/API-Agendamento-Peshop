0548253a181fd276dc862f28d01adbf299e61fc9
Ewerton Coutinho <ewerton.coutinho@alpar.com.br>
2021-08-03 15:04:54 -0300

    Streaming de dados e consumo de API com Axios

---
+++ infraestrutura/arquivos/updaloadDeArquivos.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
+
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+
+    if (tipoEhValido) {
+
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+        fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callBackImagemCriada(false, novoCaminho))
+
+    } else {
+        const erro = 'Tipo é inválido'
+        console.log('Erro! Tipo inválido')
+        callBackImagemCriada(erro)
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+    }
+}
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -3,14 +3,23 @@ class Tabelas {
         this.conexao = conexao
 
         this.criarAtendimentos()
+        this.criarPets()
         
     }
 
     criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
             erro? console.log(erro) : console.log('Tabela Atendimentos criada com sucesso')
+        })
+    }
+
+    criarPets() {
+        const sql = 'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50)  NOT NULL, imagem VARCHAR(200), PRIMARY KEY(ID))'
+
+        this.conexao.query(sql, erro => {
+            erro? console.log(erro) : console.log('Tabela Pets criada com sucesso')
         })
     }
 
